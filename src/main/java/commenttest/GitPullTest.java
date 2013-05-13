f86b02f5c4a704525c21c8b16454e76cc9d685a0
musienko.maxim@gmail.com <>
2013-05-13 11:45:29 +0000

    new tst

---
+++ src/main/java/commenttest/GitPullTest.java
@@ -0,0 +1,20 @@
+package commenttest;
+
+import java.util.ArrayList;
+
+public class GitPullTest
+{
+
+   private ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+   public GitPullTest()
+   {
+      numbers.add(1);
+      numbers.add(2);
+      numbers.add(3);
+      numbers.add(4);
+      numbers.add(5);
+      numbers.add(6);
+   }
+
+}
