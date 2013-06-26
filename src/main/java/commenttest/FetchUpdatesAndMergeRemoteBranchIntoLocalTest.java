8fa787d9956d8e54e848e8a271b53660932da2f4
musienko.maxim@gmail.com <>
2013-06-26 12:46:55 +0000

    commit_for_test_1372250667533

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
- * the License, or (at your option) any later version.
+//1372250667533_<!--change content-->
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
