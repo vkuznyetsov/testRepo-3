bb33cfb193007ffc46d687cfe59fae1fbd21e244
exoinvitemain@gmail.com <>
2013-06-28 13:36:32 +0000

    commit_for_test_1372426452400

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
- *
+//1372426452400_<!--change content-->
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
