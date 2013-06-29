a1c7dcd42a61c6bc3bc68478081c866c903abd94
exoinvitemain@gmail.com <>
2013-06-29 18:11:48 +0000

    add new file for test1372529309098

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
- * This software is distributed in the hope that it will be useful,
+//1372529309098_<!--change content-->
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
