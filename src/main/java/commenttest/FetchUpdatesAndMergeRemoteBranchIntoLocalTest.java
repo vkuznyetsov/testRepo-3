6c97547026b1f73b1d1a5fe5a8986061ba0d368e
exoinvitemain@gmail.com <>
2013-07-01 17:59:31 +0000

    commit_for_test_1372701426138

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//1372701426138_<!--change content-->
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
