cae36650a963da1c20851f48da8a2d2cbf5be047
musienko.maxim@gmail.com <>
2013-07-01 09:17:26 +0000

    commit_for_test_1372670146645

--- src/main/java/commenttest/PushingChangesTest.java
+++ src/main/java/commenttest/PushingChangesTest.java
@@ -1,4 +1,4 @@
-//1372669791675_<!--change content-->
+//1372670146645_<!--change content-->
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-1372669791675_<!--change content-->
+1372670146645_<!--change content-->
 <%
    response.sendRedirect("spring/hello");
 %>
