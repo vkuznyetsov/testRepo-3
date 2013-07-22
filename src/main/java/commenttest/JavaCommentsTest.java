e7d6b2e29cf5b40b8ac5d4ee5f86d9fcbfef2bd9
musienko.maxim@gmail.com <>
2013-07-22 08:13:56 +0000

    commitcoomit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first change1374480026933
+//first change1374480722717
  * the License, or (at your option) any later version.
  *
  * This software is distributed in the hope that it will be useful,
