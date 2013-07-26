62635b12aab7201a0f61a3c6b1370751858fd0bd
exoinvitemain@gmail.com <>
2013-07-26 18:28:12 +0000

    commit_for_test_1374863191116

--- src/main/java/commenttest/PushingChangesTest.java
+++ src/main/java/commenttest/PushingChangesTest.java
@@ -1,4 +1,4 @@
-//1374689936989_<!--change content-->
+//1374863191116_<!--change content-->
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-1374689936989_<!--change content-->
+1374863191116_<!--change content-->
 <%
    response.sendRedirect("spring/hello");
 %>
