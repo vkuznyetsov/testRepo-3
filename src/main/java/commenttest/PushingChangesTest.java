4f61ca879e52f4e8aca462a4ef95a14a0158b8d5
exoinvitemain@gmail.com <>
2013-08-16 22:13:33 +0000

    commit_for_test_1376691128699

--- src/main/java/commenttest/PushingChangesTest.java
+++ src/main/java/commenttest/PushingChangesTest.java
@@ -1,4 +1,4 @@
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//1376691128699_<!--change content-->
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-1376524970195_<!--change content-->
+1376691128699_<!--change content-->
 <%
    response.sendRedirect("spring/hello");
 %>
