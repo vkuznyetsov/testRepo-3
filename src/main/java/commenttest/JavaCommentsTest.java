b0dc8124b1527c021721924ab9e4818b3b5c0cbd
musienko.maxim@gmail.com <>
2013-08-21 09:51:00 +0000

    commitcoomit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first change1377078152993
+//first change1377078560912
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
