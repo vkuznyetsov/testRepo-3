7be3b30dc5cc36daec968708c51c5c2c9a85260c
exoinvitemain@gmail.com <>
2013-09-08 15:20:23 +0000

    commitcoomit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first change1378569571051
+//first change1378653549430
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
