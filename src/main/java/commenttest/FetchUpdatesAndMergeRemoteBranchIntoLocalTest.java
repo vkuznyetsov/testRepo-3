e6ffefac3987eb477dea904c891be1434d83f2a1
musienko.maxim@gmail.com <>
2013-10-17 07:33:47 +0000

    commit_for_test_1381995018479

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
-//1381957509412_<!--change content-->
+//1381995018479_<!--change content-->
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
