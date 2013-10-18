fefb11c374712a918f8933a28c471df380b8b2e1
musienko.maxim@gmail.com <>
2013-10-18 09:06:45 +0000

    commit_for_test_46546

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,5 +1,4 @@
-//1382085501601_<!--change content-->
- * You should have received a copy of the GNU Lesser General Public
+//1382086747628 * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
