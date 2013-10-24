dd569e74404b279b77bf5c1caca237739ef55f8b
main codenvy <exoinvitemain@gmail.com>
2013-10-24 20:11:51 +0000

    commitcommit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first change1382039984331
+//first change1382645409101
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
