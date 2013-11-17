41d53a6bfdeb1f80a858a2a6318ec6b146d13269
main codenvy <exoinvitemain@gmail.com>
2013-11-17 20:48:07 +0000

    commit_for_test_1384721104210

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
-//1384634874808_<!--change content-->
+//1384721104210_<!--change content-->
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
