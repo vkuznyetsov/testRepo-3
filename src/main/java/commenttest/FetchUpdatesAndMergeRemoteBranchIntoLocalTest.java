c0616262aae810da59da4700b9841f866c666f16
Maxim Musienko <musienko.maxim@gmail.com>
2013-12-16 12:06:44 +0000

    commit_for_test_1387195439150

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
-//1387194016951_<!--change content-->
+//1387195439150_<!--change content-->
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
