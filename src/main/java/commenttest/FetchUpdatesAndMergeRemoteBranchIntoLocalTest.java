c969177bc602339c849b33ecbc6c4dc8d50e6c63
main codenvy <exoinvitemain@gmail.com>
2013-12-18 21:00:10 +0000

    commit_for_test_1387400235593

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
-//1387226778647_<!--change content-->
+//1387400235593_<!--change content-->
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
