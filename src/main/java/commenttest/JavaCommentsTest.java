031c750954fae5e10404613cbc8e3adc28c5437c
FIRST_NAMEdwadawd LAST_NAME <exoinvitemain@gmail.com>
2014-03-04 02:19:26 +0000

    commitcommit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first change1393736666375
+//first change1393899443279
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
