b46bb1197e6bd08a51c1e429f2574e122aaa6690
max musienko <musienko.maxim@gmail.com>
2014-03-21 12:13:54 +0000

    commit_for_test_1395403798106

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
-//1395371281924_<!--change content-->
+//1395403798106_<!--change content-->
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
