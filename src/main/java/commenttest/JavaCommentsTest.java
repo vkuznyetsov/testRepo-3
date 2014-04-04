f6bdeb5437e3caaad9b7074bee2994c4530dfa1c
exo bvmbn,n <exoinvitemain@gmail.com>
2014-04-04 00:04:20 +0000

    commitcommit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first change1396466923942
+//first change1396569736481
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
