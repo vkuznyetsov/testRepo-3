9d3eb587c91cd4409af69a000e96595ea09d98eb
codenvy exomain <exoinvitemain@gmail.com>
2014-06-17 23:52:28 +0000

    commit_for_test_1403048974068

--- src/main/java/commenttest/PushingChangesTest.java
+++ src/main/java/commenttest/PushingChangesTest.java
@@ -1,4 +1,4 @@
-//1402537034833_<!--change content-->
+//1403048974068_<!--change content-->
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
