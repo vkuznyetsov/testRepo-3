47fd2b3c0cd235733a5f9d137e03ac7341c3b89e
codenvy exomain <exoinvitemain@gmail.com>
2014-06-22 21:36:58 +0000

    commit_for_test_1403472850615

--- src/main/java/commenttest/PushingChangesTest.java
+++ src/main/java/commenttest/PushingChangesTest.java
@@ -1,4 +1,4 @@
-//1403280509957_<!--change content-->
+//1403472850615_<!--change content-->
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-1403280509957_<!--change content-->
+1403472850615_<!--change content-->
 <%
    response.sendRedirect("spring/hello");
 %>
