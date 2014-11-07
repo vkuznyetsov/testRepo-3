3626e861e60358d2f027263fe786eea5fcb5131e
codenvy <iedexmain@gmail.com>
2014-11-07 15:46:04 +0200

    1415367780014

--- src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
+++ src/main/java/commenttest/FetchUpdatesAndMergeRemoteBranchIntoLocalTest.java
@@ -1,4 +1,4 @@
-//1409909050605_change_content
+//1415367780014_change_content
 /*
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
