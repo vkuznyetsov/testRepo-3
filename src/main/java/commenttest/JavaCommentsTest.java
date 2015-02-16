55933952a368407041281e7e49c39c4f88e0278d
iedexmain <iedexmain@gmail.com>
2015-02-16 21:42:09 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1424096890511
+//first_change1424122818236
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
