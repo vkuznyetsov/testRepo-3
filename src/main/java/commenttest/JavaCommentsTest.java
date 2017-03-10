f7604f6dfaf3d920357fd757c9b288d771710076
idexmai <iedexmain@gmail.com>
2017-03-10 10:52:30 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1489093429234
+//first_change1489140658336
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
