1d397347743afa66d65cd7a70fb1ca5026224e15
vkuznyetsov@codenvy.com <iedexmain@gmail.com>
2017-03-17 08:19:26 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1489678533986
+//first_change1489735258525
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
